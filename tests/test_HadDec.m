b8dfb0b599ecd9b6092072993b6ad24f1404346d
StefanoSicilia <157381946+StefanoSicilia@users.noreply.github.com>
2026-05-04 16:48:14 +0200

    Add files via upload

--- tests/test_HadDec.m
+++ tests/test_HadDec.m
@@ -1,1 +1,118 @@
+%% Script to test HadDec
 
+    %% Choice of the example type
+    m=40;
+    n=m;
+    r=floor(sqrt(min(m,n)));
+    eta=1e-4;
+    nmax=4;
+    example='random';
+    rng(1)
+    switch example
+        case 'random'
+            X=rand(m,n);
+        case 'randexact'
+            W1=randi(nmax,m,r);
+            H1=randi(nmax,n,r);
+            W2=randi(nmax,m,r);
+            H2=randi(nmax,n,r);
+            X=(W1*H1').*(W2*H2');
+        case 'randexactpert'
+            W1=randi(nmax,m,r);
+            H1=randi(nmax,n,r);
+            W2=randi(nmax,m,r);
+            H2=randi(nmax,n,r);
+            X=(W1*H1').*(W2*H2')+eta*randi(nmax,m,n);
+        case 'zeroperts'
+            X=zeros(m,n);
+            X(m,:)=ones(n,1);
+            W1=[ones(1,r); zeros(m-1,r)];
+            H1=[ones(1,r); zeros(n-1,r)];
+            W2=[ones(1,r); zeros(m-1,r)];
+            H2=[ones(1,r); zeros(n-1,r)];
+        otherwise
+            error('Example type not available.')
+    end
+
+    %% Methods parameters
+    maxit=1e6;
+    tol=1e-16;
+    Iter_W=4;
+    Iter_H=4;
+    opts=struct('maxit',maxit,'init','all','tau',0.95,'theta',1e-4,...
+        'Iter_W',Iter_W,'Iter_H',Iter_H,'tol',tol,'sparsity',0,'noloops',0);
+    if strcmp(opts.init,'given')
+        opts.W1=W1; opts.H1=H1; opts.W2=W2; opts.H2=H2;   
+    end
+    opts.momentum=[0.75,1,1.05,1.01,1.5,0.65];
+    opts.maxtime=1;
+    methods={'Manopt','manBCD','projBCD','BCD','TSVD'};
+    n_methods=length(methods)-1;
+    W1=cell(n_methods,1);
+    W2=cell(n_methods,1);
+    H1=cell(n_methods,1);
+    H2=cell(n_methods,1);
+    times=zeros(n_methods+1,1);
+    relerr=zeros(n_methods+1,1);
+    info=cell(n_methods+1,1);
+
+    %% Rank-r HDs
+    for k=1:n_methods
+        opts.method=methods{k};
+        tic;
+        [W1{k},H1{k},W2{k},H2{k},info{k}]=HadDec(X,r,opts);
+        times(k)=toc;
+        relerr(k)=info{k}.err(end);
+    end
+
+    %% TSVD of rank 2r
+    r2=min([n,m,2*r]);
+    Xsvd=X/norm(X,'fro');
+    tic;
+    [U,S,V]=svd(Xsvd);
+    times(end)=toc;
+    Sr2=S(1:r2,1:r2);
+    Ur2=U(:,1:r2);
+    Vr2=V(:,1:r2);
+    relerr(end)=norm(Xsvd-Ur2*Sr2*Vr2','fro');
+   
+    %% Display the results
+    close all
+    figure
+    lw=1.3;
+    plotsettings={'m-','r-','b-','k-'};
+
+    % Plot objective functions versus iteration 
+    for k=1:n_methods
+        semilogy(info{k}.err,plotsettings{k},'LineWidth',lw)
+        hold on
+    end
+    f=relerr(end);
+    maxl=1;
+    for k=1:n_methods
+        err_length=length(info{k}.err);
+        if err_length>maxl
+            maxl=err_length;
+        end
+    end
+    semilogy([0,maxl],[f,f],'-.','Color',[0.75,0.5,0],'LineWidth',lw)
+    hold on
+    legend(methods,'Location','best')
+
+    % Plot objective functions versus time
+    figure
+    for k=1:n_methods
+        semilogy(info{k}.time,info{k}.err,plotsettings{k},'LineWidth',lw)
+        hold on
+    end
+    f=relerr(end);
+    maxt=0;
+    for k=1:n_methods
+        time_method=info{k}.time(end);
+        if time_method>maxt
+            maxt=time_method;
+        end
+    end
+    semilogy([0,maxt],[f,f],'-.','Color',[0.75,0.5,0],'LineWidth',lw)
+    hold on
+    legend(methods,'Location','best')
---
+++ tests/test_images.m
@@ -0,0 +1,236 @@
+%% Script to test some images 
+% It takes 
+% The computational time is approximately given by 
+% n_methods*opts.maxtime*nex*nranks
+
+    %% Methods parameters and initializations
+    maxit=1e6;
+    tol=1e-16;
+    Iter_W=2;
+    Iter_H=2;
+    opts=struct('maxit',maxit,'init','all','tau',0.95,'theta',1e-4,...
+        'Iter_W',Iter_W,'Iter_H',Iter_H,'tol',tol,'sparsity',1,'noloops',1);
+    opts.momentum=[0.75,1,1.05,1.01,1.5,0.6];
+    opts.maxtime=240;
+
+    methods={'Manopt','manBCD','projBCD','BCD','TSVD'};
+    n_methods=length(methods)-1;
+    models={'cameraman','cat',...
+        'dog1','dog2','dog3','football','olivettifaces'};
+    nex=length(models);
+    nranks=2;
+    X=cell(nex,1);
+    Xsvd=cell(nex,1);
+    W1=cell(nex,n_methods,nranks);
+    H1=cell(nex,n_methods,nranks);
+    W2=cell(nex,n_methods,nranks);
+    H2=cell(nex,n_methods,nranks);
+    tableinfo=cell(nex,15,nranks);
+    errSVD=zeros(nex,nranks);
+    relerr=zeros(nex,n_methods+1,nranks);
+    info=cell(nex,n_methods,nranks);
+    ranks=zeros(nex,1);
+    startex=1;
+
+    %% Load the datasets
+    for i=startex:nex
+        switch models{i}
+            case 'cameraman'
+                X{i}=double(imread('cameraman.tif'));
+            case 'cat'
+                X{i}=double(imread('./datasets/cat.jpg'));
+            case {'dog1','dog2','dog3'}
+                string=['./datasets/',models{i},'.mat'];
+                X{i}=load(string).data;
+            case 'football'
+                X{i}=load('./datasets/football.mat').A;
+            case 'olivettifaces'
+                X{i}=load('./datasets/olivettifaces.mat').faces;
+        end
+        ranks(i,2)=floor(sqrt(min(size(X{i}))));
+        ranks(i,1)=round(ranks(i,2)/2);
+    end
+
+    %% Main computations
+    tic
+    for i=startex:nex
+        fprintf('%i) %s ...',i,models{i})
+        p=ranks(i,1);
+        r=ranks(i,2);
+        [m,n]=size(X{i});
+
+        % HDs
+        for j=1:n_methods
+            fprintf(' %s ...',methods{j})
+            opts.method=methods{j};
+            [W1{i,j,1},H1{i,j,1},W2{i,j,1},H2{i,j,1},info{i,j,1}]=...
+                HadDec(X{i},p,opts);
+            relerr(i,j,1)=info{i,j,1}.err(end);
+            [W1{i,j,2},H1{i,j,2},W2{i,j,2},H2{i,j,2},info{i,j,2}]=...
+                HadDec(X{i},r,opts);
+            relerr(i,j,2)=info{i,j,2}.err(end);
+        end
+
+        % TSVD
+        p2=2*p;
+        r2=min([m,n,2*r]);
+        Xsvd{i}=X{i}/norm(X{i},'fro');
+        [U,S,V]=svds(Xsvd{i},r2);
+        errSVD(i,1)=norm(Xsvd{i}-U(:,1:p)*S(1:p,1:p)*V(:,1:p)','fro');
+        errSVD(i,2)=norm(Xsvd{i}-U*S*V','fro');
+
+        relerr(i,end,1)=errSVD(i,1);
+        relerr(i,end,2)=errSVD(i,2);
+        tableinfo(i,1:4,1)={models{i} m n p};
+        tableinfo(i,1:4,2)={models{i} m n r};
+        k=5;
+        for j=1:n_methods
+            tableinfo(i,k:k+1,1)={info{i,j,1}.err(end) info{i,j,1}.init};
+            tableinfo(i,k:k+1,2)={info{i,j,2}.err(end) info{i,j,2}.init};
+            k=k+2;
+        end
+        tableinfo(i,end-2,1)={errSVD(i,1)};
+        tableinfo(i,end-2,2)={errSVD(i,2)};
+        fprintf(' done!\n')
+    end
+    time_largedata=toc;
+
+
+    %% Plot results for the errors
+    close all
+    lw=1.3;
+    for j=1:n_methods
+        figure
+        for i=startex:nex
+            rng(10*i)
+            semilogy(info{i,j,2}.time,info{i,j,2}.err,'-',...
+                'Color',[rand,rand,rand],'LineWidth',lw)
+            hold on
+        end
+        title(methods{j})
+        legend(models{startex:nex},'Location','best');
+    end
+
+    %% SVD improvement
+    % For each dataset, find the rank for which the rank-r TSVD provides a 
+    % lower relative error than the best HadDec (among manBCD and proj).
+
+    hadbest=min(relerr(:,1:end-1,:),[],2);
+    rstar_vec=2*ranks;
+    ratio=zeros(nex,nranks);
+    fprintf('SVD improvements checks...')
+    for i=startex:nex
+        for h=1:nranks
+            err_star=hadbest(i,h);
+            err_SVD=errSVD(i,h);
+            rstar=rstar_vec(i,h);
+            if err_star>err_SVD
+                % rank-2r TSVD is better than rank-r HD
+                while err_star>err_SVD && rstar>1
+                    rstar=rstar-1;
+                    [U,S,V]=svds(Xsvd{i},rstar);
+                    err_SVD=norm(Xsvd{i}-U*S*V','fro');
+                end
+            else
+                % rank-2r TSVD is worse than rank-r HD
+                while err_star<err_SVD && rstar<min(size(X{i}))
+                    rstar=rstar+1;
+                    [U,S,V]=svds(Xsvd{i},rstar);
+                    err_SVD=norm(Xsvd{i}-U*S*V','fro');
+                end
+            end
+            rstar_vec(i,h)=rstar;
+            ratio(i,h)=100*(rstar-2*ranks(i,h))/(2*ranks(i,h));
+            tableinfo(i,end-1:end,h)={rstar_vec(i,h),ratio(i,h)};
+        end
+    end
+    fprintf(' done!\n')
+
+    %% Show images of cameraman and dog
+    % Cameraman
+    normX=norm(X{1},'fro');
+    sq=255/normX;
+    for j=1:n_methods
+        figure
+        imshow(sq*(W1{1,j,2}*H1{1,j,2}').*(W2{1,j,2}*H2{1,j,2}'))
+        title(methods{j})
+    end
+    figure
+    [U,S,V]=svds(Xsvd{1},ranks(1,2));
+    imshow(sq*U*S*V'*normX)
+    title(methods{end})
+    figure
+    imshow(sq*X{1})
+    title('original')
+
+    % Dog
+    sq=255;
+    A=cell(n_methods+1);
+    for j=1:n_methods
+        figure
+        for k=1:3
+            normX=norm(X{k+2},'fro');
+            A{j}(:,:,k)=((W1{k+2,j,2}*H1{k+2,j,2}').*(W2{k+2,j,2}*H2{k+2,j,2}'))/normX;
+        end
+        imshow(sq*A{j})
+        title(methods{j})
+    end
+    
+    %%
+    Xsvd_dog=zeros([size(X{3}),3]);
+    figure
+    for k=1:3
+        [U,S,V]=svds(Xsvd{k+2},ranks(k+2,2));
+        A{end}(:,:,k)=(U*S*V');
+        normX(k)=norm(X{k+2},'fro');
+        Xsvd_dog(:,:,k)=Xsvd{k+2};
+    end
+    imshow(sq*A{end})
+    title(methods{end})
+    figure
+    imshow(sq*Xsvd_dog)
+    title('original')
+
+    %% Savings and percent replacement
+    relerr=100*relerr;
+    tableinfo(:,[5:2:end-3,end-2],:)=...
+        cellfun(@(x) 100*x,tableinfo(:,[5:2:end-3,end-2],:),...
+        'UniformOutput', false);
+
+    save('./results\images_info','info')
+    save('./results\images_table','tableinfo')
+
+    for j=1:n_methods+1
+        figure(j+4)
+        title('')
+        figstring=['./results./Images/cameraman_',methods{j},'.fig'];
+        pngstring=['./results./Images/cameraman_',methods{j},'.png'];
+        imwrite(frame2im(getframe(gca)),pngstring);
+        title(methods{j})
+        saveas(gcf,figstring)
+    end
+    figure(n_methods+6)
+    title('')
+    figstring='./results./Images/cameraman_original.fig';
+    pngstring='./results./Images/cameraman_original.png';
+    imwrite(frame2im(getframe(gca)),pngstring);
+    title('original')
+    saveas(gcf,figstring)
+
+    for j=1:n_methods+1
+        figure(j+n_methods+6)
+        title('')
+        figstring=['./results./Images/dog_',methods{j},'.fig'];
+        pngstring=['./results./Images/dog_',methods{j},'.png'];
+        imwrite(frame2im(getframe(gca)),pngstring);
+        title(methods{j})
+        saveas(gcf,figstring)
+    end
+    figure(2*n_methods+8)
+    title('')
+    figstring='./results./Images/dog_original.fig';
+    pngstring='./results./Images/dog_original.png';
+    imwrite(frame2im(getframe(gca)),pngstring);
+    title('original')
+    saveas(gcf,figstring)
+    
